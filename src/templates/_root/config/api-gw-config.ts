0fde7d74a5f5f5ff71354a19f772fe9bcd386dc1
Kolja Gralla <mail@koljagralla.de>
2023-12-14 13:33:34 +0100

    minor change to comments in generated file

--- src/templates/_root/config/api-gw-config.ts
+++ src/templates/_root/config/api-gw-config.ts
@@ -12,6 +12,6 @@ export const generatedCodeDisclaimer: GeneratedCodeDisclaimer = {
   purpose: 'Declares the config for the generated API Gateway.',
   expectedSignature: ['export const apiGwConfig: apigateway.RestApiProps'],
   canBeEdited: Editability.INTENDED_IF_SIGNATURE_REMAINS,
-  canBePermanentlyDeleted: Deletability.NO_CDK_SYNTH_WILL_FAIL,
+  canBePermanentlyDeleted: Deletability.NO_REGENERATED_CODE_WILL_BREAK,
   willBeRecreated: RecreationBehavior.ON_TURBOGATE_BUILD_WHEN_DELETED_WITH_BOILERPLATE,
 };
