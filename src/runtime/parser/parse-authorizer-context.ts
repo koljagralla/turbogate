f39d42d1ad77d1816faf0024bfefa548b231013f
Kolja Gralla <mail@koljagralla.de>
2023-12-18 15:33:48 +0100

    added context desirialization

--- src/runtime/parser/parse-authorizer-context.ts
+++ src/runtime/parser/parse-authorizer-context.ts
@@ -5,7 +5,8 @@ function parseAuthorizerContext<C>(zContext: ZodType<C>, event: RawRequest) {
   try {
     // @ts-ignore
     const serializedContext = event.requestContext?.authorizer?.serializedContext;
-    return zContext.parse(serializedContext);
+    const deserializedContext = JSON.parse(serializedContext);
+    return zContext.parse(deserializedContext);
   } catch (error: any) {
     throw new Error(`Failed to parse authorizer context: ${error.message}`);
   }
