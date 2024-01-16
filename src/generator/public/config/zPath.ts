ec3c87cec75873393f9a57a1008e746f299b6780
Kolja Gralla <mail@koljagralla.de>
2024-01-16 10:40:51 +0100

    added validation for path names in turbospec

--- src/generator/public/config/zPath.ts
+++ src/generator/public/config/zPath.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const zPath = z.string(); // TODO add regex (must begin with / )
+const zPath = z.string().startsWith('/'); 
 
 export type Path = z.TypeOf<typeof zPath>;
 export { zPath };
