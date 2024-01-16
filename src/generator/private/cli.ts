f88e0c4ff39a14973a9b03dbf9e456632332c72c
Kolja Gralla <mail@koljagralla.de>
2024-01-16 10:31:57 +0100

    fixed endpointStructure cli flag

--- src/generator/private/cli.ts
+++ src/generator/private/cli.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { Constants } from '../../constants';
 import { ApiBuilder, ApiBuilderConfig } from '../public/generator/api-builder';
 import { buildSpec } from '../public/generator/spec-builder';
+import { z } from 'zod';
 
 const program = new Command();
 
@@ -18,7 +19,7 @@ program
   .option(
     '-r --root <root>',
     'The directory to use as root folder',
-    process.cwd(), // Default value for root
+    process.env.INIT_CWD || process.cwd(), // Default value for root
   )
   .option(
     '-s --spec <spec>',
@@ -28,10 +29,16 @@ program
   // TODO limit to byResource and byPath
   .option(
     '-e --endpointStructure <endpointStructure>',
-    'Whether to put all endpoint folders directly in the endpoints folder (e.g. endpoints/my-operation, recommend for small APIs) or to infer the resource from the path and create an organizational hierarchy level between endpoints and operation folder (e.g. endpoints/my-resource/my-operation, recommend for larger APIs). Migrating generated code between this options is tedious work so choose wisely.',
+    'Whether to put all endpoint folders directly in the endpoints folder ("allTogether", e.g. endpoints/my-operation, recommend for small APIs) or to infer the resource from the path and create an organizational hierarchy level between endpoints and operation folder ("byResource", e.g. endpoints/my-resource/my-operation, recommend for larger APIs). Migrating generated code between this options is tedious work so choose wisely.',
     'byResource', // Default value for endpointStructure
   )
   .action(async args => {
+    const endpointStructure = z
+      .enum(['allTogether', 'byResource'], {
+        invalid_type_error:
+          'Only "allTogether" and "byResource" are allowed values for option --endpointStructure (-e)!',
+      })
+      .parse(args.endpointStructure);
     const apiBuilderConfig: ApiBuilderConfig = {
       rootDirectory: path.isAbsolute(args.root) ? args.root : path.join(process.cwd()!, args.root),
       configFileName: args.spec,
