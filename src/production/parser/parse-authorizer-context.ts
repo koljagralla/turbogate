d41e3e2b5cb8f1ab7358c1bd50fe3744548176b0
Kolja Gralla <mail@koljagralla.de>
2024-01-23 19:29:28 +0100

    fixed bug in parseAuthorizerContext

--- src/production/parser/parse-authorizer-context.ts
+++ src/production/parser/parse-authorizer-context.ts
@@ -5,7 +5,7 @@ function parseAuthorizerContext<C>(zContext: ZodType<C>, event: RawRequest) {
   try {
     // @ts-ignore
     const serializedContext = event.requestContext?.authorizer?.serializedContext;
-    const deserializedContext = JSON.parse(serializedContext);
+    const deserializedContext = serializedContext ? JSON.parse(serializedContext) : undefined;
     return zContext.parse(deserializedContext);
   } catch (error: any) {
     throw new Error(`Failed to parse authorizer context: ${error.message}`);
