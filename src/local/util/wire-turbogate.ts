a9636233bf88ae932f5e06b9fbe41b2dec77181b
Kolja Gralla <mail@koljagralla.de>
2024-01-24 12:31:30 +0100

    added docs

--- src/local/util/wire-turbogate.ts
+++ src/local/util/wire-turbogate.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod';
 import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
 
-// Ensure Zod is extended with the openapi method
+/**
+ * This file only exists to be able to ensure that some necessary prototype alterations 
+ * can be conducted at the very beginning of CDK synth. Just calling the code below in the 
+ * entrypoint of the CDK app will not work, as imports will already have been resolved and
+ * during those imports some code will be executed that will not have the prototype alterations.
+ */
+
 extendZodWithOpenApi(z);
 
 export const wireTurbogate = undefined;
