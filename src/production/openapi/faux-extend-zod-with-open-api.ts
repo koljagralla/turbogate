361ed413d113b7bec1c9bad6eea27814be1b6f86
Kolja Gralla <mail@koljagralla.de>
2024-01-24 09:57:42 +0100

    fixed fauxExtend and extend methods conflicting during synth

--- src/production/openapi/faux-extend-zod-with-open-api.ts
+++ src/production/openapi/faux-extend-zod-with-open-api.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 
-
 /**
  * This mocks the extendZodWithOpenApi method from @asteasolutions/zod-to-openapi. We neither need nor want the
  * zod-to-openapi functions in production code. However, we do want to use the openapi method in our data model
@@ -14,8 +13,12 @@ function fauxExtendZodWithOpenApi(zod: typeof z): void {
   };
 }
 
-// This gets called when this file imported in the handler.ts
-fauxExtendZodWithOpenApi(z);
+// We only want to faux openapi extension in lambda context. Locally we will extend zod with the actual openapi method.
+const isRunningInLambdaContext = !!(process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV);
+if (isRunningInLambdaContext) {
+  // This gets called when this file imported in the handler.ts
+  fauxExtendZodWithOpenApi(z);
+}
 
 // Export something so we can import this file in the handler.ts
 export const handleFauxExtendZodWithOpenApi = undefined;
