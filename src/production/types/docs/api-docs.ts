86c73bdb10e8b5af766ee74eb86859a6e2af58a1
Kolja Gralla <mail@koljagralla.de>
2024-01-31 15:22:19 +0100

    fixed ApiDocs type definition

--- src/production/types/docs/api-docs.ts
+++ src/production/types/docs/api-docs.ts
@@ -1,3 +1,3 @@
 import { OpenAPIObjectConfigV31 } from '@asteasolutions/zod-to-openapi/dist/v3.1/openapi-generator';
 
-export type ApiDocs = Pick<OpenAPIObjectConfigV31, 'info' | 'externalDocs' | 'tags' | 'servers'>;
+export type ApiDocs = Omit<OpenAPIObjectConfigV31, 'openapi' | 'paths' | 'components' | 'webhooks' | 'security'>;
